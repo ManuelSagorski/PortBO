98f93ffaa6da412589948387589b18cc647269cc
Gordon Dahlke <gordonniklasdahlke@gmail.com>
2022-05-12 16:43:58 +0200

    PMB-5 Übersetzung Impressum: Impressum-Links in den Footern relativ gemacht (vorher absolute Links zu https://port-mission.de/bo/impressum.php)

--- bo/views/templates/_indexSiteFooter.template.php
+++ bo/views/templates/_indexSiteFooter.template.php
@@ -1,6 +1,6 @@
 		</div>
 	    <div id="footer">
-        	<a onclick="loadPwReset();">Passwort vergessen</a> | <a href="https://port-mission.de/bo/impressum.php" target="_blank">Impressum</a>
+        	<a onclick="loadPwReset();">Passwort vergessen</a> | <a href="/bo/impressum.php" target="_blank">Impressum</a>
         </div>
 	</body>
 </html>
